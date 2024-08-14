cb7979a93bc3a01232c4843c79b31c11a62db775
Dariusz Depta <dariusz@confio.gmbh>
2024-08-14 12:42:04 +0200

    Updates.

--- mte-blocks/tests/multitest/test_block.rs
+++ mte-blocks/tests/multitest/test_block.rs
@@ -4,7 +4,7 @@ use cw_multi_test::App;
 fn default_block_should_work() {
     let app = App::default();
     let block = app.block_info();
-    assert_eq!(1571797419879305533, block.time.nanos());
+    assert_eq!(1571797419879305533, block.time.nanos()); // Wed Oct 23 2019 02:23:39 GMT+0000
     assert_eq!(12345, block.height);
     assert_eq!("cosmos-testnet-14002", block.chain_id);
 }
