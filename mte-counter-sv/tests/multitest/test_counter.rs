dfacc35254953cbafb6bc9dd6b0ad85613a5ba36
Dariusz Depta <dariusz@confio.gmbh>
2024-08-21 11:32:46 +0200

    Updates.

--- mte-counter-sv/tests/multitest/test_counter.rs
+++ mte-counter-sv/tests/multitest/test_counter.rs
@@ -11,9 +11,6 @@ fn instantiating_should_work() {
     let contract = code_id.instantiate().call(&owner).unwrap();
 
     assert_eq!(0, contract.count().unwrap().count);
-
-    contract.increment().call(&owner).unwrap();
-    assert_eq!(1, contract.count().unwrap().count);
 }
 
 #[test]
@@ -26,4 +23,18 @@ fn incrementing_should_work() {
 
     contract.increment().call(&owner).unwrap();
     assert_eq!(1, contract.count().unwrap().count);
+}
+
+#[test]
+fn incrementing_should_stop_at_255() {
+    let app = App::default();
+    let code_id = CodeId::store_code(&app);
+
+    let owner = "owner".into_addr();
+    let contract = code_id.instantiate().call(&owner).unwrap();
+
+    for _ in 0..300 {
+        contract.increment().call(&owner).unwrap();
+    }
+    assert_eq!(255, contract.count().unwrap().count);
 }
