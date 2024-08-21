ff3a84e4112b49e6e88ec4cb1779f9e8da35a4f9
Dariusz Depta <dariusz@confio.gmbh>
2024-08-21 10:31:43 +0200

    Fixed examples.

--- mte-counter-sv/src/bin/schema.rs
+++ mte-counter-sv/src/bin/schema.rs
@@ -1,8 +1,7 @@
 use cosmwasm_schema::write_api;
 
-use counter_sylvia::contract::sv::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
+use counter::contract::sv::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
 
-#[cfg(not(tarpaulin_include))]
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
