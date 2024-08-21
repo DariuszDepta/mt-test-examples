e9876fcfcd41996e49f308f79933882462ee02ab
Dariusz Depta <dariusz@confio.gmbh>
2024-08-21 09:41:25 +0200

    Updates.

--- mte-app/tests/multitest/test_app_default.rs
+++ mte-app/tests/multitest/test_app_default.rs
@@ -1,13 +1,15 @@
-use cw_multi_test::{no_init, App};
-
 #[test]
 fn creating_default_app_should_work() {
+    use cw_multi_test::App;
+
     let app = App::default();
     assert_eq!(12345, app.block_info().height);
 }
 
 #[test]
 fn creating_new_app_should_work() {
+    use cw_multi_test::{no_init, App};
+
     let app = App::new(no_init);
     assert_eq!(12345, app.block_info().height);
 }
