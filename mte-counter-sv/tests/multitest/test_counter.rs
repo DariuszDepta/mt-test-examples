486e8a43fc60519bd5d5eb01412ca7c23540f00f
Dariusz Depta <dariusz@confio.gmbh>
2024-08-21 11:06:43 +0200

    Refactored Sylvia counter example.

--- mte-counter-sv/tests/multitest/test_counter.rs
+++ mte-counter-sv/tests/multitest/test_counter.rs
@@ -1,6 +1,4 @@
-#![cfg(test)]
-
-use crate::contract::sv::mt::{CodeId, CounterContractProxy};
+use counter::contract::sv::mt::{CodeId, CounterContractProxy};
 use sylvia::cw_multi_test::IntoAddr;
 use sylvia::multitest::App;
 
@@ -8,9 +6,12 @@ use sylvia::multitest::App;
 fn instantiate() {
     let app = App::default();
     let code_id = CodeId::store_code(&app);
-    let owner = "owner".into_addr();
+
+    let owner ="owner".into_addr();
     let contract = code_id.instantiate().call(&owner).unwrap();
+
     assert_eq!(0, contract.count().unwrap().count);
+
     contract.increment().call(&owner).unwrap();
     assert_eq!(1, contract.count().unwrap().count);
 }
