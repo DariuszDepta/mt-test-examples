595fb1179783f1dfdfb7a8dfbe152b76b3e346e1
Dariusz Depta <dariusz@confio.gmbh>
2024-09-12 13:29:35 +0200

    Updates.

--- mte-addresses/tests/multitest/test_app_api.rs
+++ mte-addresses/tests/multitest/test_app_api.rs
@@ -12,7 +12,7 @@ fn creating_address_with_default_prefix_should_work() {
 }
 
 #[test]
-fn creating_address_with_custom_prefix_should_work() {
+fn creating_bech32_address_with_custom_prefix_should_work() {
     use cw_multi_test::{no_init, AppBuilder, MockApiBech32};
 
     let app = AppBuilder::default()
@@ -22,6 +22,21 @@ fn creating_address_with_custom_prefix_should_work() {
     let addr = app.api().addr_make("owner");
     assert_eq!(
         "juno1fsgzj6t7udv8zhf6zj32mkqhcjcpv52yph5qsdcl0qt94jgdckqsmg2ndy",
+        addr.as_str()
+    );
+}
+
+#[test]
+fn creating_bech32m_address_with_custom_prefix_should_work() {
+    use cw_multi_test::{no_init, AppBuilder, MockApiBech32m};
+
+    let app = AppBuilder::default()
+        .with_api(MockApiBech32m::new("juno"))
+        .build(no_init);
+
+    let addr = app.api().addr_make("owner");
+    assert_eq!(
+        "juno1fsgzj6t7udv8zhf6zj32mkqhcjcpv52yph5qsdcl0qt94jgdckqsw56lgx",
         addr.as_str()
     );
 }
