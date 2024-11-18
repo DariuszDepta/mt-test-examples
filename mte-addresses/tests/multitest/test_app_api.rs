7af9a3febfa57b625ce9fb7dc1cc26104857cc77
Dariusz Depta <dariusz@confio.gmbh>
2024-11-18 12:48:51 +0100

    Updates.

--- mte-addresses/tests/multitest/test_app_api.rs
+++ mte-addresses/tests/multitest/test_app_api.rs
@@ -80,3 +80,65 @@ fn rule_6() {
         addr.as_str()
     );
 }
+
+#[test]
+#[allow(unused_variables)]
+fn init_1() {
+    use cw_multi_test::App;
+
+    let app = App::new(|router, api, storage| {
+        let addr = api.addr_make("owner");
+        assert_eq!(
+            "cosmwasm1fsgzj6t7udv8zhf6zj32mkqhcjcpv52yph5qsdcl0qt94jgdckqs2g053y",
+            addr.as_str()
+        );
+    });
+}
+
+#[test]
+#[allow(unused_variables)]
+fn init_2() {
+    use cw_multi_test::{AppBuilder, MockApiBech32};
+
+    let app = AppBuilder::default()
+        .with_api(MockApiBech32::new("nebula"))
+        .build(|router, api, storage| {
+            let addr = api.addr_make("owner");
+            assert_eq!(
+                "nebula1fsgzj6t7udv8zhf6zj32mkqhcjcpv52yph5qsdcl0qt94jgdckqsvsqrvp",
+                addr.as_str()
+            );
+        });
+}
+
+#[test]
+#[allow(unused_variables)]
+fn init_3() {
+    use cw_multi_test::{AppBuilder, MockApiBech32m};
+
+    let app = AppBuilder::default()
+        .with_api(MockApiBech32m::new("nebula"))
+        .build(|router, api, storage| {
+            let addr = api.addr_make("owner");
+            assert_eq!(
+                "nebula1fsgzj6t7udv8zhf6zj32mkqhcjcpv52yph5qsdcl0qt94jgdckqsevs0fr",
+                addr.as_str()
+            );
+        });
+}
+
+#[test]
+#[allow(unused_variables)]
+fn init_4() {
+    use cw_multi_test::{AppBuilder, MockApiBech32m};
+
+    let app = AppBuilder::default()
+        .with_api(MockApiBech32m::new("cosmwasm"))
+        .build(|router, api, storage| {
+            let addr = api.addr_make("owner");
+            assert_eq!(
+                "cosmwasm1fsgzj6t7udv8zhf6zj32mkqhcjcpv52yph5qsdcl0qt94jgdckqsl5lc5x",
+                addr.as_str()
+            );
+        });
+}
